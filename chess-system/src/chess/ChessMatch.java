2090073e75913878e4eec3a9bf9d402ca73b2871
LucaoCorrea <lucascorrea1505@gmail.com>
2025-05-13 14:57:08 -0300

    captured and printcaptured

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -2,6 +2,9 @@ package chess;
 
 import chess.Color;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -12,6 +15,9 @@ public class ChessMatch {
     private Color currentPlayer;
     private Board board;
     private boolean check;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch() {
         board = new Board(8, 8);
@@ -62,6 +68,12 @@ public class ChessMatch {
         Piece p = board.removPiece(source);
         Piece capturedPiece = board.removPiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
+
         return capturedPiece;
     }
 
@@ -91,6 +103,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup() {
